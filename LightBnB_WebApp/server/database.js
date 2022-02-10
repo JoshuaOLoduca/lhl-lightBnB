7b1b592bc254165938dcb15062347b2c3cff7aab
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-02-10 13:48:49 -0500

    removed debugging code

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -185,9 +185,6 @@ const getAllProperties = function (options, limit = 10) {
   ORDER BY cost_per_night
   LIMIT $${queryParams.length};
   `;
-
-  console.log(queryString, queryParams);
-  console.log(options);
   
   return pool.query(queryString, queryParams)
     .then((result) => result.rows)
