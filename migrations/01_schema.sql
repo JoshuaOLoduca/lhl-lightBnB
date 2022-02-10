994794c678a8c86dabd83450f38a86673a3b4e6e
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-02-10 10:53:52 -0500

    added more tables for reviewing guests and by date rates

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -2,6 +2,8 @@ DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS properties CASCADE;
 DROP TABLE IF EXISTS reservations CASCADE;
 DROP TABLE IF EXISTS property_reviews CASCADE;
+DROP TABLE IF EXISTS guest_reviews CASCADE;
+DROP TABLE IF EXISTS rates CASCADE;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
@@ -12,7 +14,8 @@ CREATE TABLE users (
 
 CREATE TABLE properties (
   id SERIAL PRIMARY KEY NOT NULL,
-  owner_id INTEGER REFERENCES users(id)
+  owner_id INTEGER
+    REFERENCES users(id)
     ON DELETE CASCADE,
   title VARCHAR(255) NOT NULL,
   description TEXT,
@@ -36,20 +39,50 @@ CREATE TABLE reservations (
   id SERIAL PRIMARY KEY NOT NULL,
   start_date DATE NOT NULL,
   end_date DATE NOT NULL,
-  property_id INTEGER REFERENCES properties(id)
+  property_id INTEGER
+    REFERENCES properties(id)
     ON DELETE CASCADE,
-  guest_id INTEGER REFERENCES users(id)
+  guest_id INTEGER
+    REFERENCES users(id)
     ON DELETE CASCADE
 );
 
 CREATE TABLE property_reviews (
   id SERIAL PRIMARY KEY NOT NULL,
-  guest_id INTEGER REFERENCES users(id)
+  guest_id INTEGER
+    REFERENCES users(id)
     ON DELETE CASCADE,
-  property_id INTEGER REFERENCES properties(id)
+  property_id INTEGER
+    REFERENCES properties(id)
     ON DELETE CASCADE,
-  reservation_id INTEGER REFERENCES reservations(id)
+  reservation_id INTEGER
+    REFERENCES reservations(id)
     ON DELETE CASCADE,
   rating SMALLINT NOT NULL DEFAULT 0,
   message TEXT
+);
+
+CREATE TABLE guest_reviews (
+  id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER
+    REFERENCES users(id)
+    ON DELETE CASCADE,
+  guest_id INTEGER
+    REFERENCES users(id)
+    ON DELETE CASCADE,
+  reservation_id INTEGER
+    REFERENCES reservations(id)
+    ON DELETE CASCADE,
+  rating SMALLINT NOT NULL DEFAULT 0,
+  message TEXT
+);
+
+CREATE TABLE rates (
+  id SERIAL PRIMARY KEY NOT NULL,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  cost_per_night INTEGER NOT NULL DEFAULT 0,
+  property_id INTEGER
+    REFERENCES properties(id)
+    ON DELETE CASCADE
 );
